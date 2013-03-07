b371dc7b9c3ad9e237da25e7ffd4da4911413968
Erik Nilsson <enilsson@compassion.com.au>
2013-03-07 16:44:53 +1100

    Removed Code Snippets from Code files and placed in snippets.txt

--- ChildPoolListings/Controllers/ChildListingsController.cs
+++ ChildPoolListings/Controllers/ChildListingsController.cs
@@ -10,24 +10,13 @@ namespace ChildPoolListings.Controllers
         // GET: /ChildListings/GetChildren/
         public JsonResult GetChildren()
         {
-            using (var session = WebApiApplication.DocumentStore.OpenSession())
-            {
-                var results = session.Query<RegisteredChild>()
-                    .Where(c => c.MarkAsDeleted == false)
-                    .Where(c => c.RequestLockId == Guid.Empty)
-                    .ToArray();
-                return Json(results, JsonRequestBehavior.AllowGet);
-            }
+            throw new NotImplementedException();
         }
 
         // GET: /ChildListings/GetChild/
         public JsonResult GetChild(string childId)
         {
-            using (var session = WebApiApplication.DocumentStore.OpenSession())
-            {
-                var result = session.Query<RegisteredChild>().FirstOrDefault(c => c.ChildId == Guid.Parse(childId));
-                return Json(result, JsonRequestBehavior.AllowGet);
-            }
+            throw new NotImplementedException();
         }
     }
 }
--- SponsorAChild/Controllers/HomeController.cs
+++ SponsorAChild/Controllers/HomeController.cs
@@ -16,74 +16,22 @@ namespace SponsorAChild.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
-            var client = new HttpClient();
-            var response = client.GetAsync("http://localhost:59677/ChildListings/GetChildren/").Result;
-            var content = response.Content;
-            var children = new DataContractJsonSerializer(typeof(IEnumerable<Child>)).ReadObject(content.ReadAsStreamAsync().Result);
-            return View(children);
+            throw new NotImplementedException();
         }
 
         // GET: /Home/SponsorChild/
         public ActionResult SponsorChild(string childId)
         {
-            var sessionId = Guid.NewGuid();
-            Session["SessionId"] = sessionId;
-            var childIdGuid = Guid.Parse(childId);
-            var client = new HttpClient();
-            var response = client.GetAsync("http://localhost:59677/ChildListings/GetChild/?childId=" + childIdGuid).Result;
-            var content = response.Content;
-            var child = new DataContractJsonSerializer(typeof(Child)).ReadObject(content.ReadAsStreamAsync().Result);
-
-            var command = new RegisterIntentToSponsor 
-            { 
-                ChildId = childIdGuid,
-                SessionId = sessionId 
-            };
-
-            var successful = MvcApplication.Bus.Send(command)
-                .Register(CommandResponseCallback, this)
-                .AsyncWaitHandle.WaitOne(5000);
-
-            if (!successful)
-            {
-                ViewBag.Successful = false;
-                ViewBag.ErrorReason = "Timed Out";
-            }
-
-            return View(child);
+            throw new NotImplementedException();
         }
 
         public ActionResult ConfirmSponsorship(string childId)
         {
-            var sessionId = (Guid)Session["SessionId"];
-            var childIdGuid = Guid.Parse(childId);
-
-            var command = new VerifyIntentToSponsor
-            {
-                ChildId = childIdGuid, 
-                SessionId = sessionId
-            };
-
-            var successful = MvcApplication.Bus.Send(command)
-                .Register(CommandResponseCallback, this)
-                .AsyncWaitHandle.WaitOne(5000);
-
-            if (!successful)
-            {
-                ViewBag.Successful = false;
-                ViewBag.ErrorReason = "Timed Out";
-            }
-
-            return View("ConfirmSponsorship");
+            throw new NotImplementedException();
         }
 
         private void CommandResponseCallback(IAsyncResult asyncResult)
         {
-            var result = (CompletionResult)asyncResult.AsyncState;
-            var controller = (HomeController)result.State;
-            var response = result.Messages.Cast<ChildRequestResponse>().First();
-            controller.ViewBag.ErrorReason = response.Reason;
-            controller.ViewBag.Successful = response.Successful;
         }
     }
 }
